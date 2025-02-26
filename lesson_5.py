8497edef77dc39c5669404b5a5e732ba7050d22e
V1aD1slavee <vladboulderchannel@gmail.com>
2025-02-26 13:35:40 +0200

    Update lesson_5.py

--- lesson_5.py
+++ lesson_5.py
@@ -1,4 +1,19 @@
 """
+Для первого пуша
+git init
 git add .
-git remote add origin 
+git remote add origin <git link>
+git commit -m "First commit"
+git push
+git branch -M main
+
+git push -u origin <Название метки>
+git push -u origin main
+"""
+
+"""
+Комманды для сущ репозитория
+git add .
+git commit -m "коментарий к коммиту"
+git push
 """
