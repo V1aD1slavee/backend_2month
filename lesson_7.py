356f55502fd521e45cabf4b727b550b7a0668c9e
V1aD1slavee <vladboulderchannel@gmail.com>
2025-03-20 21:22:41 +0200

    added func 'add_student'

--- lesson_7.py
+++ lesson_7.py
@@ -16,3 +16,16 @@ CREATE TABLE IF NOT EXISTS animators(
                cuts DOUBLE (5, 2) DEFAULT 0.0,
                is_married BOOLEAN DEFAULT False)""")
 
+def add_student():
+    user_full_name = input("Введите имя: ")
+    user_hobby = input("Введите ваше хобби: ")
+    user_phone_number = int(input("Введите номер телефона: "))
+    user_birthday = input("Введите дату рождения: ")
+
+    cursor.execute(f"""INSERT INTO animators
+                   (full_name, hobby, phone_number, birth_date)
+                   VALUES ('{user_full_name}', '{user_hobby}', {user_phone_number}, '{user_birthday}')""")
+
+    connect.commit()
+
+add_student()
