92314d097701ece4d63e8eefd711d915e3074f1b
V1aD1slavee <vladboulderchannel@gmail.com>
2025-03-20 18:28:16 +0200

    created Data Base

--- lesson_7.py
+++ lesson_7.py
@@ -1,2 +1,16 @@
 "БД - Базы Данных"
 "СУБД - Система Управления Базами Данных"
+
+import sqlite3
+
+connect = sqlite3.connect("Animation.db") # Подключение к базе данных
+cursor = connect.cursor()
+
+cursor.execute("""
+CREATE TABLE animators(
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               full_name VARCHAR (25),
+               hobby TEXT,
+               phone_number INT,
+               birth_day DATE,
+)""")
