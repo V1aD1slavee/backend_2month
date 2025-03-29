3ad1b301f446955774887b89e43c423479eb3ced
V1aD1slavee <vladboulderchannel@gmail.com>
2025-03-29 21:20:15 +0200

    commit

--- lesson_8.py
+++ lesson_8.py
@@ -1,7 +1,7 @@
 import sqlite3
 
-connect = sqlite3.connect("Animation.db")  # Подключение к базе данных
-cursor = connect.cursor()  # Посредник для работы с базой данных
+connect = sqlite3.connect("Animation.db")
+cursor = connect.cursor()
 
 cursor.execute(
     """
@@ -30,16 +30,76 @@ class Egypt:
         user_phone_number = int(input("Введите номер телефона: "))
         user_birthday = input("Введите дату рождения: ")
 
-        cursor.execute(
-            f"SELECT phone_number FROM animators WHERE phone_number == {user_phone_number}"
-        )
+        cursor.execute(f"SELECT phone_number FROM animators WHERE phone_number == {user_phone_number}")
         animator = cursor.fetchone()
 
         if animator:
             print(f"Пользователь с номером +{user_phone_number} уже существует!")
         else:
+            cursor.execute(f"""INSERT INTO animators
+                            (full_name, hobby, phone_number, birth_day)
+                            VALUES ('{user_full_name}', '{user_hobby}', {user_phone_number}, '{user_birthday}')""")
+
+        connect.commit()
+
+    def all_animation(self):
+        cursor.execute("SELECT * FROM animators")
+        animator = cursor.fetchall()
+        print(animator)
+
+    def delete_animator(self):
+        user_id = int(input("Введите id пользователя: "))
+        cursor.execute(f"SELECT id, full_name FROM animators WHERE id == {user_id}")
+        del_anim = cursor.fetchone()
+
+        if del_anim:
+            cursor.execute(f"DELETE FROM animators WHERE id == {user_id}")
+            connect.commit()
+            print(f"Пользователь {del_anim} удалён!")
+        else:
+            print("Такого пользователя нет в базе данных")
+
+    def update_animator(self):
+        user_id = int(input("Введите id пользователя: "))
+        user_full_name = input("Введите новое имя: ")
+        user_hobby = input("Введите новое хобби: ")
+        user_phone_number = int(input("Введите новый номер телефона: "))
+        user_birthday = input("Введите новую дату рождения: ")
+        user_cut = float(input("Сколько штрафов?: "))
+        user_is_married = bool(input("Пользователь в браке? 1 - да 0 - нет: "))
+
+        cursor.execute(f"SELECT id, full_name FROM animators WHERE id == {user_id}")
+        anim = cursor.fetchone()
+
+        if anim:
             cursor.execute(
-                f"""INSERT INTO animators
-                        (full_name, hobby, phone_number, birth_day)
-                        VALUES ('{user_full_name}', '{user_hobby}', {user_phone_number}, '{user_birthday}')"""
+                """UPDATE animators SET 
+                full_name = ? , hobby = ?, phone_number = ?,
+                birth_day = ?, cuts = ?, is_married = ? WHERE id = ?""",(
+                user_full_name,
+                user_hobby,
+                user_phone_number,
+                user_birthday,
+                user_cut,
+                user_is_married,
+                user_id)
             )
+
+            connect.commit()
+            print(f"Пользователь {anim} обновлён!")
+        else:
+            print(f"Пользователь с таким id не существует")
+
+    def update_animators_cut(self):
+        user_id = int(input("Введите id пользователя: "))
+        user_cut = float(input("Введите количевство штрафов пользователя: "))
+        cursor.execute(f"SELECT id, full_name FROM animators WHERE id == {user_id}")
+        anim = cursor.fetchone()
+
+        if anim:
+            cursor.execute("""UPDATE animators SET cuts = ? + ? WHERE id = ?""",
+                           (user_cut, user_cut, user_id))
+            connect.commit()
+            print(f"Кол-во штрафов пользователя {anim} успешно изменено")
+        else:
+            print("Такого пльзователя не существует")
